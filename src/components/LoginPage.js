7bb9e6771b541051a9911a36c9524aadd9d763e8
aaronshivers <aaron@moonmail.org>
2019-08-05 15:54:40 -0500

    customized login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import { Container, Col } from 'react-bootstrap'
+import { Container, Col, Card } from 'react-bootstrap'
 import LoginLogoutButton from './LoginLogoutButton'
 
 const LoginPage = () => (
   <div className="login-page">
     <Container>
-    <Col xs={ { span: 6, offset: 3 } }>
-      <div className="text-center">
-        <h1>
-          Login
-        </h1>
-        <LoginLogoutButton />
-      </div>
-    </Col>
+      <Col sm={ { span: 6, offset: 3 } }>
+        <Card className="text-center pt-3 pb-3 bg-semi-transparent">
+          <Card.Body>
+            <h2>
+              Timesheet
+            </h2>
+            <LoginLogoutButton />
+          </Card.Body>
+        </Card>
+      </Col>
     </Container>
   </div>
 )
--- src/components/NewJobPage.js
+++ src/components/NewJobPage.js
@@ -1,42 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Container, Form, Button } from 'react-bootstrap'
+import Navigation from './Navigation'
 
 const NewJobPage = () => {
   const [ customer, setCustomer ] = useState('')
   const [ description, setDescription ] = useState('')
 
+  useEffect(() => console.log(customer, description))
+
   return (
-    <Container>
-      <h1>New Job</h1>
-      <Form>
-        <Form.Group controlId="customer">
-          <Form.Label>Customer</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Customer..."
-            value={ customer }
-            onChange={ e => setCustomer(e.target.value) }
+    <>
+      <Navigation />
+      <Container>
+        <h1>New Job</h1>
+        <Form>
+          <Form.Group controlId="customer">
+            <Form.Label>Customer</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Customer..."
+              value={ customer }
+              onChange={ e => setCustomer(e.target.value) }
+            >
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="description">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Description..."
+              value={ description }
+              onChange={ e => setDescription(e.target.value) }
+            >
+            </Form.Control>
+          </Form.Group>
+          <Button
+            type="submit"
+            size="lg"
           >
-          </Form.Control>
-        </Form.Group>
-        <Form.Group controlId="description">
-          <Form.Label>Description</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Description..."
-            value={ description }
-            onChange={ e => setDescription(e.target.value) }
-          >
-          </Form.Control>
-        </Form.Group>
-        <Button
-          type="submit"
-          size="lg"
-        >
-          Add Job
-        </Button>
-      </Form>
-    </Container>
+            Add Job
+          </Button>
+        </Form>
+      </Container>
+    </>
   )
 }
 
