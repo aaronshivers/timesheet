04c5e8dac9e075d4c0b3e2e6fce278ed200d301e
aaron shivers <aaorn@moonmail.org>
2019-08-05 12:28:03 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const App = () => (
+  <div>
+    <Container>
+      <h1 className="display-1 text-center">timesheet-v2</h1>
+    </Container>
+  </div>
+)
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: ['./src/app.js'],
+  output: {
+    path: path.join(__dirname, 'public', 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader:'babel-loader'
+      }, {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          }, {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }, {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+          },
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    })
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true,
+    publicPath: '/dist/'
+  }
+}
