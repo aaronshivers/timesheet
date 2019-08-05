7550fcb20b12aa8e196de214af1a150351bd3fd3
aaronshivers <aaron@moonmail.org>
2019-08-05 14:58:33 -0500

    added Navigation component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
 import React, { useContext } from 'react'
 import { Container, Button } from 'react-bootstrap'
 import Context from '../context/context'
-import LoginLogoutButton from './LoginLogoutButton'
+import Navigation from './Navigation'
 
 const App = () => {
   const { uid } = useContext(Context)
 
   return (
-    <div>
+    <>
+      <Navigation />
       <Container>
-        <h1 className="display-1 text-center">timesheet-v2</h1>
-        <LoginLogoutButton uid={ uid } />
         <Button href="/newjob">New Job</Button>
       </Container>
-    </div>
+    </>
   )
 }
+
 export default App
