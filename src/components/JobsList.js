17a89e4582b09272c6ea3cf157dec8138c90f11e
aaronshivers <aaron@moonmail.org>
2019-08-06 08:49:17 -0500

    added card around job list

--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { ListGroup } from 'react-bootstrap'
+import { Card, CardHeader, ListGroup } from 'react-bootstrap'
 import Context from '../context/context'
 import DeleteButton from './DeleteButton'
 import { getJobs } from '../actions/jobs'
@@ -14,20 +14,23 @@ const JobsList = () => {
   }, [])
 
   return (
-    <ListGroup className="mt-3">
-      {
-        state && state.map(({ id, customer, description }) => (
-          <ListGroup.Item key={ id }>
-            <Link to={ `/editjob/${ id }` }>
-              <h3>Customer</h3>
-              <span className="text-break align-middle">{ customer }</span>
-              <h4>Job Description</h4>
-              <span className="text-break align-middle">{ description }</span>
-            </Link>
-          </ListGroup.Item>
-        ))
-      }
-    </ListGroup>
+    <Card className="mt-3">
+      <Card.Header>Jobs</Card.Header>
+      <ListGroup variant="flush">
+        {
+          state && state.map(({ id, customer, description }) => (
+            <ListGroup.Item key={ id }>
+              <Link to={ `/editjob/${ id }` }>
+                <h3>Customer</h3>
+                <span className="text-break align-middle">{ customer }</span>
+                <h4>Job Description</h4>
+                <span className="text-break align-middle">{ description }</span>
+              </Link>
+            </ListGroup.Item>
+          ))
+        }
+      </ListGroup>
+    </Card>
   )
 }
 
