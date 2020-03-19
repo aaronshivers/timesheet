46beecc1a27e345036b423656c6b55e2623fad5f
aaronshivers <aaron@moonmail.org>
2020-03-19 05:06:19 -0500

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const Dotenv = require('dotenv-webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
@@ -44,7 +43,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'styles.css'
     }),
-    new Dotenv()
   ],
   devtool: 'inline-source-map',
   devServer: {
