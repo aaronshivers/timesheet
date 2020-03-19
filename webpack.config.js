6e5549b3141d8649badd1eb972b28dfe62b886c6
aaronshivers <aaron@moonmail.org>
2020-03-19 05:24:17 -0500

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,36 @@
+const { EnvironmentPlugin } = require('webpack')
+
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: ['@babel/polyfill', './src/app.js'],
+  entry: [ '@babel/polyfill', './src/app.js' ],
   output: {
-    path: path.join(__dirname, 'public', 'dist')
+    path: path.join(__dirname, 'public', 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader:'babel-loader'
+        loader: 'babel-loader',
       }, {
         test: /\.s?css$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader
+            loader: MiniCssExtractPlugin.loader,
           }, {
             loader: 'css-loader',
             options: {
-              sourceMap: true
-            }
+              sourceMap: true,
+            },
           }, {
             loader: 'sass-loader',
             options: {
-              sourceMap: true
-            }
-          }
-        ]
+              sourceMap: true,
+            },
+          },
+        ],
       }, {
         test: /\.(png|jpg|gif)$/i,
         use: [
@@ -36,18 +38,27 @@ module.exports = {
             loader: 'url-loader',
           },
         ],
-      }
-    ]
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'styles.css'
+      filename: 'styles.css',
     }),
+    new EnvironmentPlugin([
+      'FIREBASE_API_KEY',
+      'FIREBASE_AUTH_DOMAIN',
+      'FIREBASE_DATABASE_URL',
+      'FIREBASE_PROJECT_ID',
+      'FIREBASE_MESSAGING_SENDER_ID',
+      'FIREBASE_APP_ID',
+      'FIREBASE_STORAGE_BUCKET',
+    ]),
   ],
   devtool: 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     historyApiFallback: true,
-    publicPath: '/dist/'
-  }
+    publicPath: '/dist/',
+  },
 }
